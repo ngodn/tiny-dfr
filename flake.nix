21b09e73e37568b5700cdeba76c97125d6928700
Nick Breitling <breitling.nw@gmail.com>
2025-05-13 23:16:12 -0500

    updated nix flake
    Signed-off-by: Nick Breitling <breitling.nw@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,30 +1,21 @@
 {
   description = "The most basic dynamic function row daemon possible";
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11"; };
   outputs = { self, nixpkgs }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       pkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-    in
-    rec {
-      packages = forAllSystems(system: 
-        let
-          pkgs = pkgsFor.${system};
-        in
-        {
+    in rec {
+      packages = forAllSystems (system:
+        let pkgs = pkgsFor.${system};
+        in {
           default = pkgs.rustPlatform.buildRustPackage {
-            name = "tiny-dfr";
-            version = "0.2.0";
+            pname = "tiny-dfr";
+            version = "0.3.4";
             src = ./.;
-            cargoLock = {
-              lockFile = ./Cargo.lock;
-            };
-            nativeBuildInputs = [
-              pkgs.pkg-config
-            ];
+            cargoLock = { lockFile = ./Cargo.lock; };
+            nativeBuildInputs = [ pkgs.pkg-config ];
             buildInputs = [
               pkgs.cairo
               pkgs.libinput
@@ -34,27 +25,30 @@
               pkgs.pango
               pkgs.gdk-pixbuf
               pkgs.libxml2
+              pkgs.librsvg
             ];
-          };
-        }
-      );
 
-      devShells = forAllSystems(system:
-        let
-          pkgs = pkgsFor.${system};
-        in
-        {
+            postConfigure = ''
+              substituteInPlace etc/systemd/system/tiny-dfr.service \
+                  --replace-fail /usr/bin $out/bin
+              substituteInPlace src/*.rs --replace-quiet /usr/share $out/share
+            '';
+
+            postInstall = ''
+              cp -R etc $out/lib
+              cp -R share $out
+            '';
+          };
+        });
+
+      devShells = forAllSystems (system:
+        let pkgs = pkgsFor.${system};
+        in {
           default = pkgs.mkShell {
-            inputsFrom = [
-              packages.${system}.default
-            ];
-            packages = [
-    	        pkgs.rustfmt
-              pkgs.rust-analyzer
-            ];
+            inputsFrom = [ packages.${system}.default ];
+            packages = [ pkgs.rustfmt pkgs.rust-analyzer ];
             RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
           };
-        }
-      );
+        });
     };
 }
